cbe305e4295c36d955010cea9cf16bf6f194922e
Branca Chiotte <brancachiotte@gmail.com>
2020-12-04 19:57:51 +0000

    finished

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ const App = () => {
   return (
     <div className="ui container comments">
       <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
         <CommentDetail
           author="Sam"
           timeAgo="Today at 4:45PM"
@@ -15,18 +21,22 @@ const App = () => {
           avatar={faker.image.people()}
         />
       </ApprovalCard>
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00AM"
-        content="Nice Job!"
-        avatar={faker.image.people()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 5:00PM"
-        content="Proud!"
-        avatar={faker.image.people()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          content="Nice Job!"
+          avatar={faker.image.people()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          content="Proud!"
+          avatar={faker.image.people()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
